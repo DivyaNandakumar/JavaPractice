8cba5bfc19879a863c6a14b8987a65c2848472e8
Divya N <divya.n@myntra.com>
2022-02-03 16:23:57 +0530

    reverse string

---
+++ problems/src/main/java/easyProblems/reverseString.java
@@ -0,0 +1,40 @@
+package easyProblems;
+
+import java.util.Arrays;
+
+public class reverseString {
+
+    static String reverse(String str) {
+
+        char[] reversedstring = str.toCharArray();
+        char temp;
+        int backindex=str.length()-1;
+        int length=str.length();
+        for (int index = 0; index <= length / 2; index++) {
+            temp=reversedstring[backindex];
+            reversedstring[backindex]=reversedstring[index];
+            reversedstring[index]=temp;
+            backindex--;
+        }
+
+        StringBuilder stb=new StringBuilder("");
+        stb.append(reversedstring);
+        return stb.toString();
+    }
+
+    static String reversewithBuilder(String str) {
+
+        StringBuilder stb=new StringBuilder(str);
+        return stb.reverse().toString();
+    }
+
+    public static void main(String[] args){
+
+        System.out.println(reverse("hello there"));
+
+        System.out.println(reversewithBuilder("who are you"));
+
+    }
+
+
+}
