3040cc031bd42385c5801e9f8c7f40ca135c294d
Divya N <divya.n@myntra.com>
2022-03-15 00:32:59 +0530

    code for duplicate check

--- problems/src/main/java/Arrays/ContainsDuplicate.java
+++ problems/src/main/java/Arrays/ContainsDuplicate.java
@@ -1,10 +1,14 @@
 package Arrays;
 
+import javax.naming.PartialResultException;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate {
     /*
-    Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-
-
+    Given an integer array nums,
+    return true if any value appears at least twice in the array,
+    and return false if every element is distinct.
 
 Example 1:
 
@@ -26,4 +30,20 @@ Constraints:
 -109 <= nums[i] <= 109
 
      */
+
+
+    public static void main(String args[]){
+        int[] array1={1,4,5,6,8,9,0};
+        Set<Integer> duplicatecheck=new HashSet<Integer>();
+        int index=0;
+        for(index=0;index<array1.length;index++){
+            if(!duplicatecheck.add(array1[index])){
+                System.out.println("false");
+                break;
+            }
+        }
+        if(index>=array1.length) {
+            System.out.println("true");
+        }
+    }
 }
