c458ae076f753eea283c77e3168855d59646b5b6
Divya N <divya.n@myntra.com>
2022-03-16 13:49:42 +0530

    code;

--- problems/src/main/java/Arrays/LongestWord.java
+++ problems/src/main/java/Arrays/LongestWord.java
@@ -1,5 +1,37 @@
 package Arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class LongestWord {
     // determining the largest word in a string.
+    //Solutions:
+    //1. convert string to arraylist splitting based on space
+    //2. we just need to get th length of of each element of the list
+
+
+    static int length=0;
+    static String longestword="";
+
+    public static void  main(String args[]){
+
+        String str= "Hello my friendsof me. How are you amazingly folks doing";
+        ArrayList<String> list= new ArrayList<String>(Arrays.asList(str.split(" ",0)));
+
+        System.out.println(list);
+
+        list.forEach((element)->{
+            if(element.length()>length){
+                setlongestword(element);
+            }
+        });
+        System.out.println("longest word is :" + longestword);
+    }
+
+    public static void setlongestword(String element){
+        length=element.length();
+        longestword=element;
+    }
+
+
 }
